177fb4a0189dd5fb95010817f08a47765cbdb34c
Dawn-githup <13477199767@163.com>
2023-11-10 12:34:20 +0800

    Fix issue with reading configuration files

--- spore-sdk/config/predefined.ts
+++ spore-sdk/config/predefined.ts
@@ -1,10 +1,11 @@
-import { predefined } from '@ckb-lumos/config-manager';
 import { SporeConfig } from './types';
 import {readFileSync} from "fs";
+import * as fs from 'fs';
 
 export type PredefinedSporeConfigScriptName = 'Spore' | 'Cluster';
 
 export const LUMOS_CONFIG_PATH = "lumos.json"
+const jsonData = JSON.parse(fs.readFileSync(LUMOS_CONFIG_PATH, 'utf8'));
 
 function getLocalConfig(): any {
   const config = readFileSync(LUMOS_CONFIG_PATH, 'utf-8');
@@ -20,12 +21,12 @@ const TESTNET_SPORE_CONFIG: SporeConfig<PredefinedSporeConfigScriptName> = {
   scripts: {
     Spore: {
       script: {
-        codeHash: '0xf142459eb940a0a5a270ab3b4baf223d30236e5950b342fe6a050f735efc231f',
+        codeHash: jsonData.SCRIPTS.SPORE.CODE_HASH,
         hashType: 'type',
       },
       cellDep: {
         outPoint: {
-          txHash: '0x6d86a7f259930c0bd2c7167f41581b4d9d5f8b1d52513934d6b216f2ada48ff1',
+          txHash: jsonData.SCRIPTS.SPORE.TX_HASH,
           index: '0x0',
         },
         depType: 'code',
@@ -34,12 +35,12 @@ const TESTNET_SPORE_CONFIG: SporeConfig<PredefinedSporeConfigScriptName> = {
     },
     Cluster: {
       script: {
-        codeHash: '0x1bf1c14596c3f22c60a0018c0373e37674e3a3ce0bec902359a461f51259ae98',
+        codeHash: jsonData.SCRIPTS.SPORE_CLUSTER.CODE_HASH,
         hashType: 'type',
       },
       cellDep: {
         outPoint: {
-          txHash: '0xa38a2b62f75cbdcba4f55236abf6fd3e72ba5f41ecd22d1bbbfa73fe1f9f8a8f',
+          txHash: jsonData.SCRIPTS.SPORE_CLUSTER.TX_HASH,
           index: '0x0',
         },
         depType: 'code',
